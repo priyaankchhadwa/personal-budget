6b996897d962edd8c5e29378d6b7c6025b500639
priyaankchhadwa <priyaank29@gmail.com>
2020-10-04 20:35:51 -0400

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -1,52 +1,54 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
 const budget = {
-    myBudget: [{
-        title: 'Eat Out',
-        budget: 50
+  myBudget: [
+    {
+      title: "Eat Out",
+      budget: 50,
     },
     {
-        title: 'Groceries',
-        budget: 80
+      title: "Groceries",
+      budget: 80,
     },
     {
-        title: 'Movies',
-        budget: 20
+      title: "Movies",
+      budget: 20,
     },
     {
-        title: 'Games',
-        budget: 60
+      title: "Games",
+      budget: 60,
     },
     {
-        title: 'Internet',
-        budget: 30
+      title: "Internet",
+      budget: 30,
     },
     {
-        title: 'Gas',
-        budget: 25
+      title: "Gas",
+      budget: 25,
     },
     {
-        title: 'Rent',
-        budget: 300
+      title: "Rent",
+      budget: 150,
     },
     {
-        title: 'Drinks',
-        budget: 15
-    }]
-}
+      title: "Drinks",
+      budget: 15,
+    },
+  ],
+};
 
-app.use('/', express.static('public'))
+app.use("/", express.static("public"));
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!')
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/budget', (req, res) => {
-    res.json(budget)
-})
+app.get("/budget", (req, res) => {
+  res.json(budget);
+});
 
 app.listen(port, () => {
-    console.log(`example app listening at port - ${port}`)
-})
+  console.log(`example app listening at port - ${port}`);
+});
